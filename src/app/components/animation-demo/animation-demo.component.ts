7199ce257c5855ce626e7ef1641dceb6a1d05dae
Vlad Pascu <vlad_pascu25@yahoo.com>
2021-10-18 02:22:06 +0300

    dark theme

--- src/app/components/animation-demo/animation-demo.component.ts
+++ src/app/components/animation-demo/animation-demo.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { faSmileWink, faGrinStars, faLaughBeam, faGrinHearts, faGrinTongue, faDizzy } from '@fortawesome/free-regular-svg-icons';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ThemeService } from 'src/app/services';
+import { Theme } from 'src/app/services/enums';
 
 @Component({
   selector: 'app-animation-demo',
   templateUrl: './animation-demo.component.html',
   styleUrls: ['./animation-demo.component.scss']
 })
-export class AnimationDemoComponent implements OnInit {
+export class AnimationDemoComponent implements OnInit, OnDestroy {
+  public winkEmoji = faSmileWink;
+  public starsEmoji = faGrinStars;
+  public laughEmoji = faLaughBeam;
+  public heartEmoji = faGrinHearts;
+  public tongueEmoji = faGrinTongue;
+  public dizzyEmoji = faDizzy;
 
-  constructor() { }
+  public isDark = false;
+  private isDestroyed$: Subject<void> = new Subject<void>();
 
-  ngOnInit(): void {
+  constructor(private themeService: ThemeService) { }
+
+  public ngOnInit(): void {
+    this.subscribeToTheme();
   }
 
+  private subscribeToTheme(): void {
+    this.themeService.currentTheme$.pipe(
+      takeUntil(this.isDestroyed$)
+      ).subscribe((theme) => {
+      this.isDark = theme === Theme.Dark;
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.isDestroyed$.next();
+  }
 }
